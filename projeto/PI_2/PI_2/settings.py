87579984598520a9146c5e64f4304d2d014eebf0
LucasLoman <lucasloman@hotmail.com>
2024-10-03 22:11:47 -0300

    pelo jeito funcionou

--- projeto/PI_2/PI_2/settings.py
+++ projeto/PI_2/PI_2/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dbpi',
         'USER': 'postgres',
-        'PASSWORD': 'avocado123',
+        'PASSWORD': 'lucas1510',
         'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
         'PORT': '',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
