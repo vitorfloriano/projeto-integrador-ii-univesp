7de7ad3bffc097ff4dba9c27de27968103ffcbcf
GabrielFerreiraS-GFS <159969758+GabrielFerreiraS-GFS@users.noreply.github.com>
2024-11-06 16:30:11 -0300

    Mudanças no DB
    Mudanças rápidas na tabela

--- projeto/PI_2/app/admin.py
+++ projeto/PI_2/app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(empresa1)
+admin.site.register(geral)
 admin.site.register(empresa2)
 admin.site.register(empresa3)
 
--- projeto/PI_2/app/models.py
+++ projeto/PI_2/app/models.py
@@ -1,24 +1,48 @@
 from django.db import models
 
-class empresa1(models.Model):
-    title = models.CharField(max_length=80)
-    age = models.IntegerField()
+class geral(models.Model):
+    class Meta:
+        verbose_name_plural = 'Tabelas Gerais'
+    
+    nome = models.CharField(max_length=80)
+    idade = models.IntegerField()
+    CPF = models.CharField(max_length=14)
+    RG = models.CharField(max_length=12)
+    admissao = models.CharField(max_length=10)
+    cargo = models.CharField(max_length=80)
+    salario = models.CharField()
 
     def __str__(self):
-        return self.title
+        return self.nome
     
 class empresa2(models.Model):
-    title = models.CharField(max_length=80)
-    age = models.IntegerField()
-
+    class Meta:
+        verbose_name_plural = 'Exemplo Empresa 1 ----------- CNPJ 11.111.111/1111-11'
+    
+    nome = models.CharField(max_length=80)
+    idade = models.IntegerField()
+    CPF = models.CharField(max_length=14)
+    RG = models.CharField(max_length=12)
+    admissao = models.CharField(max_length=10)
+    cargo = models.CharField(max_length=80)
+    salario = models.CharField()
+    
     def __str__(self):
-        return self.title
+        return self.nome
 
 class empresa3(models.Model):
-    title = models.CharField(max_length=80)
-    age = models.IntegerField()
+    class Meta:
+        verbose_name_plural = 'Exemplo Empresa 2 ----------- CNPJ 22.222.222/2222-22'
+    
+    nome = models.CharField(max_length=80)
+    idade = models.IntegerField()
+    CPF = models.CharField(max_length=14)
+    RG = models.CharField(max_length=12)
+    admissao = models.CharField(max_length=10)
+    cargo = models.CharField(max_length=80)
+    salario = models.CharField()
 
     def __str__(self):
-        return self.title
+        return self.nome
 
 # Create your models here.
